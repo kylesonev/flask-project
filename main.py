84c94456ed2b7d5abd96f61cdebde32d8eebc190
Guilherme <mxtroy3@gmail.com>
2025-04-24 07:27:56 -0300

    Adicionado funções ao main.py

--- main.py
+++ main.py
@@ -3,9 +3,22 @@ from flask import Flask
 app = Flask(__name__)
 
 
+# Rota principal
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def home():
+    return "Bem-vindo à página inicial!"
+
+
+# Rota sobre
+@app.route("/sobre")
+def sobre():
+    return "Essa é a página sobre o projeto."
+
+
+# Rota como parâmetro
+@app.route("/usuario/<username>")
+def usuario(username):
+    return f"Bem-vindo(a) {username}!"
 
 
 if __name__ == "__main__":
